376491cc21b80121067b20e9860c5beb7d6668aa
Massinissa Mokhtari <mokhtari.massinissa@gmail.com>
2019-02-05 01:21:30 +0100

    Clean up

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,2 @@
-#include "Device.h"
 
-int main() {
-    Api::PrintInstanceLayerProperties();
-
-    VkInstance instance = Api::CreateInstance();
-    VkPhysicalDevice gpu = Api::FetchFirstPhysicalDevice(instance);
-    VkDevice device = Api::CreateLogicalDevice(gpu);
-}
+int main() {}
